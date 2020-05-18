657b2707df6c32fc947ec450c025d9773d8d3944
Tobiasz Kubiak <tobiaszkubiak31@gmail.com>
2020-05-18 15:41:30 +0200

    Check if we try to confirm after 25 hours, when order is expired

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -14,47 +14,24 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class OrderTest {
-
-    private DateTime dateTime;
-
-    @Test
-    public void checkIfTryToRealizeFromCreatedState() {
-        Order order = new Order();
-        order.addItem(new OrderItem());
-        Assertions.assertThrows(OrderStateException.class, () -> order.realize());
-    }
+    @Mock
+    private OrderItem orderItem;
 
     @Test
-    public void checkIfConfirmAfter24HoursOfSubmitShouldThrowOrderExpiredException() {
-        dateTime = DateTime.now();
+    public void checkConfirmAfter25HoursOfSubmit_shouldThrowOrderExpiredException() {
+        DateTime dateTime = DateTime.now();
         DatatimeClock clock = Mockito.mock(DatatimeClock.class);
+
         when(clock.getTime())
             .thenReturn(dateTime)
             .thenReturn(dateTime.plusHours(25));
 
         Order order = new Order(clock);
-        order.addItem(new OrderItem());
+        order.addItem(orderItem);
         order.submit();
-        order.confirm();
 
         Assertions.assertThrows(OrderExpiredException.class, () -> order.confirm());
         Assertions.assertEquals(State.CANCELLED, order.getOrderState());
     }
 
-    @Test
-    public void checkIfConfirmAfter23HoursShouldSetStateRealized() {
-        dateTime = DateTime.now();
-        DatatimeClock clock = Mockito.mock(DatatimeClock.class);
-        when(clock.getTime())
-            .thenReturn(dateTime)
-            .thenReturn(dateTime.plusHours(23));
-
-        Order order = new Order(clock);
-        order.addItem(new OrderItem());
-        order.submit();
-        order.confirm();
-        order.realize();
-
-        Assertions.assertEquals(State.REALIZED, order.getOrderState());
-    }
 }
